003599c7a1cc476456abc72e990afd0c9f012c25
giuliapl <libralessogiulia@gmail.com>
2024-02-02 23:03:16 +0100

    chore: first settings

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
-import './index.css'
+import './styles.scss'
+import "./assets/fonts/AvertaDemoPECuttedDemo-Regular.otf";
+import "./assets/fonts/AvertaDemoPE-ExtraBold.otf"
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
+
+root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
