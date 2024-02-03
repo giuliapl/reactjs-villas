584a5731022c4ebf85d82e00d1eb15a1b1ef9bca
giuliapl <libralessogiulia@gmail.com>
2024-02-03 11:57:35 +0100

    chore: gradient cover comp wip

--- src/components/gradientcover/GradientCover.tsx
+++ src/components/gradientcover/GradientCover.tsx
@@ -1,39 +1,41 @@
-import Card from '@mui/joy/Card';
-import CardCover from '@mui/joy/CardCover';
-import CardContent from '@mui/joy/CardContent';
-import Typography from '@mui/joy/Typography';
-import LocationOnRoundedIcon from '@mui/icons-material/LocationOnRounded';
+import Card from "@mui/joy/Card";
+import CardCover from "@mui/joy/CardCover";
+import CardContent from "@mui/joy/CardContent";
+import Typography from "@mui/joy/Typography";
 
 interface GradientCoverProps {
-  name: string
+  imgUrl: string;
+  title?: string;
+  description?: string;
+  alt?: string;
 }
-export default function GradientCover(props : GradientCoverProps) {
+export default function GradientCover(props: GradientCoverProps) {
   return (
-    <Card sx={{ minHeight: '320px', width: 180 }}>
+    <Card
+      sx={{
+        minHeight: "320px",
+        width: 180,
+        "&:hover": {
+          cursor: "pointer",
+          transition: "transform .2s",
+          transform: "scale(1.07)"
+        },
+      }}
+    >
       <CardCover>
-        <img
-          src="https://images.unsplash.com/photo-1542773998-9325f0a098d7?auto=format&fit=crop&w=320"
-          srcSet="https://images.unsplash.com/photo-1542773998-9325f0a098d7?auto=format&fit=crop&w=320&dpr=2 2x"
-          loading="lazy"
-          alt=""
-        />
+        <img src={props.imgUrl} loading="lazy" alt={props.alt} />
       </CardCover>
       <CardCover
- sx={{
-    background:
-      'linear-gradient(to top, rgba(0,0,0,0.4), rgba(0,0,0,0) 200px), linear-gradient(to top, rgba(0,0,0,0.8), rgba(0,0,0,0) 300px)',
-  }}
+        sx={{
+          background:
+            "linear-gradient(to top, rgba(0,0,0,0.4), rgba(0,0,0,0) 200px), linear-gradient(to top, rgba(0,0,0,0.8), rgba(0,0,0,0) 300px)",
+        }}
       />
-      <CardContent sx={{ justifyContent: 'flex-end' }}>
+      <CardContent sx={{ justifyContent: "flex-end" }}>
         <Typography level="title-lg" textColor="#fff">
-          Yosemite National Park
+          {props.title}
         </Typography>
-        <Typography
-          startDecorator={<LocationOnRoundedIcon />}
-          textColor="neutral.300"
-        >
-          {props.name}
-        </Typography>
+        <Typography textColor="neutral.300">{props.description}</Typography>
       </CardContent>
     </Card>
   );
